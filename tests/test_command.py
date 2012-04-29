027ca037da952e9a4f7a1cd6ba5d92bc1ed1eea7
Doug Hellmann <doug.hellmann@dreamhost.com>
2012-04-29 15:26:41 -0400

    100% coverage of cliff.command

---
+++ tests/test_command.py
@@ -0,0 +1,22 @@
+
+from cliff.command import Command
+
+
+class TestCommand(Command):
+    """Description of command.
+    """
+
+    def run(self, parsed_args):
+        return
+
+
+def test_get_description():
+    cmd = TestCommand(None, None)
+    desc = cmd.get_description()
+    assert desc == "Description of command.\n    "
+
+
+def test_get_parser():
+    cmd = TestCommand(None, None)
+    parser = cmd.get_parser('NAME')
+    assert parser.prog == 'NAME'
