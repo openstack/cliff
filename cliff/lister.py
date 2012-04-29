a25a7912180e4f41bb1daf4398d23033036a4386
Doug Hellmann <doug.hellmann@dreamhost.com>
2012-04-29 19:19:42 -0400

    refactor ShowOne and Lister to share a common base class

--- cliff/lister.py
+++ cliff/lister.py
@@ -3,54 +3,24 @@
 import abc
 import logging
 
-import pkg_resources
-
-from .command import Command
+from .display import DisplayCommandBase
 
 
 LOG = logging.getLogger(__name__)
 
 
-class Lister(Command):
+class Lister(DisplayCommandBase):
     """Command base class for providing a list of data as output.
     """
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, app, app_args):
-        super(Lister, self).__init__(app, app_args)
-        self.load_formatter_plugins()
+    @property
+    def formatter_namespace(self):
+        return 'cliff.formatter.list'
 
-    def load_formatter_plugins(self):
-        self.formatters = {}
-        for ep in pkg_resources.iter_entry_points('cliff.formatter.list'):
-            try:
-                self.formatters[ep.name] = ep.load()()
-            except Exception as err:
-                LOG.error(err)
-                if self.app_args.debug:
-                    raise
-
-    def get_parser(self, prog_name):
-        parser = super(Lister, self).get_parser(prog_name)
-        formatter_group = parser.add_argument_group(
-            title='output formatters',
-            description='List output formatter options',
-            )
-        formatter_choices = sorted(self.formatters.keys())
-        formatter_default = 'table'
-        if formatter_default not in formatter_choices:
-            formatter_default = formatter_choices[0]
-        formatter_group.add_argument(
-            '-f', '--format',
-            dest='formatter',
-            action='store',
-            choices=formatter_choices,
-            default=formatter_default,
-            help='the output format to use, defaults to %s' % formatter_default,
-            )
-        for name, formatter in sorted(self.formatters.items()):
-            formatter.add_argument_group(parser)
-        return parser
+    @property
+    def formatter_default(self):
+        return 'table'
 
     @abc.abstractmethod
     def get_data(self, parsed_args):
