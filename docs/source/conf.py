1f34d45e325f3e3e965b22dcf43820e3d8c4f1e6
Doug Hellmann <doug.hellmann@dreamhost.com>
2013-03-01 16:17:59 -0500

    fix version in docs
    Change-Id: I19cc26b88f80fa3a848713e94d3d1aa1a26de121
    Signed-off-by: Doug Hellmann <doug.hellmann@dreamhost.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2012, Doug Hellmann'
 # built documents.
 #
 # The short X.Y version.
-version = '1.1'
+version = '1.3.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
