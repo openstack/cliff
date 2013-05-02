2bdbbee3f49108cd03eafcc8910716df8c09ebed
Doug Hellmann <doug.hellmann@dreamhost.com>
2013-05-02 14:22:22 -0400

    declare support for python 3.3
    Signed-off-by: Doug Hellmann <doug.hellmann@dreamhost.com>

--- setup.py
+++ setup.py
@@ -157,6 +157,7 @@ setup(
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
                  'Programming Language :: Python :: 3.2',
+                 'Programming Language :: Python :: 3.3',
                  'Intended Audience :: Developers',
                  'Environment :: Console',
                  ],
