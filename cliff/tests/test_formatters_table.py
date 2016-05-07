c59f04465cff2d2d147491d5ec521e8b769bd377
Vincent Legoll <vincent.legoll@idgrilles.fr>
2016-05-07 18:33:35 +0200

    Add CLIFF_MAX_TERM_WIDTH envvar to complement --max-width
    Some cliff clients tailors their output according to characteristics
    of the terminal they outputs text to. They detect the width of the
    terminal in order to pretty print text properly cut to line length.
    
    There are situations where the output is not in a "real" terminal
    but in some kind of emulation or redirected layer for which this
    line length interpretation is not ideal. For example in a bash
    jupyter-notebook.
    
    There already exist the --max-width CLI parameter to allow that
    to be customized, but there are cases where you don't want to
    use an additional parameter. So add an environment variable
    that has the same meaning, to help avoiding the use of
    --max-width.
    
    Without the parameter nor the envvar, the behaviour should stay
    the same as before the patch, i.e. the output is cut to terminal
    line length. The envvar is then used to give the parameter's
    default value, so the parameter will stay being the final one
    deciding of the behaviour when used.
    
    Add mock'ed testing for the new CLIFF_MAX_TERM_WIDTH envvar.
    Add mock'ed tests to ensure parameter vs envvar priority.
    Add testing for actual parameter parsing & priority vs envvar.²
    
    This has been discussed in:
    https://bugs.launchpad.net/python-openstackclient/+bug/1577368
    https://bugs.launchpad.net/python-cliff/+bug/1485847/comments/7
    
    Closes-Bug: 1577368
    Related-Bug: 1485847
    
    Change-Id: I0ab988ea0e7087960a9bfb2353f46918f0333a53
    Signed-off-by: Vincent Legoll <vincent.legoll@idgrilles.fr>

--- cliff/tests/test_formatters_table.py
+++ cliff/tests/test_formatters_table.py
@@ -2,13 +2,18 @@
 
 import mock
 from six import StringIO
+import os
 
 from cliff.formatters import table
 
 
 class args(object):
     def __init__(self, max_width=0):
-        self.max_width = max_width
+        if max_width > 0:
+            self.max_width = max_width
+        else:
+            # Envvar is only taken into account iff CLI parameter not given
+            self.max_width = int(os.environ.get('CLIFF_MAX_TERM_WIDTH', 0))
 
 
 @mock.patch('cliff.utils.terminal_width')
@@ -30,6 +35,95 @@ def test_table_formatter(tw):
 '''
     output = StringIO()
     parsed_args = args()
+    sf.emit_one(c, d, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+
+@mock.patch('cliff.utils.terminal_width')
+def test_table_formatter_cli_param(tw):
+    tw.return_value = 80
+    sf = table.TableFormatter()
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Testing...')
+    sf.add_argument_group(parser)
+    parsed_args = parser.parse_args(['--max-width', '42'])
+    c = ('a', 'b', 'c', 'd')
+    d = ('A', 'B', 'C', 'd' * 77)
+    expected = '''\
++-------+--------------------------------+
+| Field | Value                          |
++-------+--------------------------------+
+| a     | A                              |
+| b     | B                              |
+| c     | C                              |
+| d     | dddddddddddddddddddddddddddddd |
+|       | dddddddddddddddddddddddddddddd |
+|       | ddddddddddddddddd              |
++-------+--------------------------------+
+'''
+    output = StringIO()
+    sf.emit_one(c, d, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+
+@mock.patch('cliff.utils.terminal_width')
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '666'})
+def test_table_formatter_cli_param_envvar_big(tw):
+    tw.return_value = 80
+    sf = table.TableFormatter()
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Testing...')
+    sf.add_argument_group(parser)
+    parsed_args = parser.parse_args(['--max-width', '42'])
+    c = ('a', 'b', 'c', 'd')
+    d = ('A', 'B', 'C', 'd' * 77)
+    expected = '''\
++-------+--------------------------------+
+| Field | Value                          |
++-------+--------------------------------+
+| a     | A                              |
+| b     | B                              |
+| c     | C                              |
+| d     | dddddddddddddddddddddddddddddd |
+|       | dddddddddddddddddddddddddddddd |
+|       | ddddddddddddddddd              |
++-------+--------------------------------+
+'''
+    output = StringIO()
+    sf.emit_one(c, d, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+
+@mock.patch('cliff.utils.terminal_width')
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '23'})
+def test_table_formatter_cli_param_envvar_tiny(tw):
+    tw.return_value = 80
+    sf = table.TableFormatter()
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Testing...')
+    sf.add_argument_group(parser)
+    parsed_args = parser.parse_args(['--max-width', '42'])
+    c = ('a', 'b', 'c', 'd')
+    d = ('A', 'B', 'C', 'd' * 77)
+    expected = '''\
++-------+--------------------------------+
+| Field | Value                          |
++-------+--------------------------------+
+| a     | A                              |
+| b     | B                              |
+| c     | C                              |
+| d     | dddddddddddddddddddddddddddddd |
+|       | dddddddddddddddddddddddddddddd |
+|       | ddddddddddddddddd              |
++-------+--------------------------------+
+'''
+    output = StringIO()
     sf.emit_one(c, d, output, parsed_args)
     actual = output.getvalue()
     assert expected == actual
@@ -225,6 +319,370 @@ def test_table_list_formatter_max_width(tw):
     # 3 columns each 8 wide, plus table spacing and borders
     expected_width = 11 * 3 + 1
     assert len(actual.splitlines()[0]) == expected_width
+
+
+# Force a wide terminal by overriding its width with envvar
+@mock.patch('cliff.utils.terminal_width')
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '666'})
+def test_table_list_formatter_max_width_and_envvar_max(tw):
+    tw.return_value = 80
+    sf = table.TableFormatter()
+    c = ('one', 'two', 'three')
+    d1 = (
+        'one one one one one',
+        'two two two two',
+        'three three')
+    data = [d1]
+    parsed_args = args()
+
+    # no resize
+    expected = '''\
++---------------------+-----------------+-------------+
+| one                 | two             | three       |
++---------------------+-----------------+-------------+
+| one one one one one | two two two two | three three |
++---------------------+-----------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+    # resize 1 column
+    tw.return_value = 50
+    expected = '''\
++---------------------+-----------------+-------------+
+| one                 | two             | three       |
++---------------------+-----------------+-------------+
+| one one one one one | two two two two | three three |
++---------------------+-----------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+    # resize 2 columns
+    tw.return_value = 45
+    expected = '''\
++---------------------+-----------------+-------------+
+| one                 | two             | three       |
++---------------------+-----------------+-------------+
+| one one one one one | two two two two | three three |
++---------------------+-----------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+    # resize all columns
+    tw.return_value = 40
+    expected = '''\
++---------------------+-----------------+-------------+
+| one                 | two             | three       |
++---------------------+-----------------+-------------+
+| one one one one one | two two two two | three three |
++---------------------+-----------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+    # resize all columns limited by min_width=8
+    tw.return_value = 10
+    expected = '''\
++---------------------+-----------------+-------------+
+| one                 | two             | three       |
++---------------------+-----------------+-------------+
+| one one one one one | two two two two | three three |
++---------------------+-----------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+
+# Force a narrow terminal by overriding its width with envvar
+@mock.patch('cliff.utils.terminal_width')
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '47'})
+def test_table_list_formatter_max_width_and_envvar_mid(tw):
+    tw.return_value = 80
+    sf = table.TableFormatter()
+    c = ('one', 'two', 'three')
+    d1 = (
+        'one one one one one',
+        'two two two two',
+        'three three')
+    data = [d1]
+    parsed_args = args()
+
+    # no resize
+    expected = '''\
++---------------+---------------+-------------+
+| one           | two           | three       |
++---------------+---------------+-------------+
+| one one one   | two two two   | three three |
+| one one       | two           |             |
++---------------+---------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+    # resize 1 column
+    tw.return_value = 50
+    expected = '''\
++---------------+---------------+-------------+
+| one           | two           | three       |
++---------------+---------------+-------------+
+| one one one   | two two two   | three three |
+| one one       | two           |             |
++---------------+---------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+    assert len(actual.splitlines()[0]) == 47
+
+    # resize 2 columns
+    tw.return_value = 45
+    expected = '''\
++---------------+---------------+-------------+
+| one           | two           | three       |
++---------------+---------------+-------------+
+| one one one   | two two two   | three three |
+| one one       | two           |             |
++---------------+---------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+    assert len(actual.splitlines()[0]) == 47
+
+    # resize all columns
+    tw.return_value = 40
+    expected = '''\
++---------------+---------------+-------------+
+| one           | two           | three       |
++---------------+---------------+-------------+
+| one one one   | two two two   | three three |
+| one one       | two           |             |
++---------------+---------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+    assert len(actual.splitlines()[0]) == 47
+
+    # resize all columns limited by min_width=8
+    tw.return_value = 10
+    expected = '''\
++---------------+---------------+-------------+
+| one           | two           | three       |
++---------------+---------------+-------------+
+| one one one   | two two two   | three three |
+| one one       | two           |             |
++---------------+---------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+    assert len(actual.splitlines()[0]) == 47
+
+
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '80'})
+def test_table_list_formatter_env_maxwidth_noresize():
+    sf = table.TableFormatter()
+    c = ('one', 'two', 'three')
+    d1 = (
+        'one one one one one',
+        'two two two two',
+        'three three')
+    data = [d1]
+    parsed_args = args()
+
+    # no resize
+    expected = '''\
++---------------------+-----------------+-------------+
+| one                 | two             | three       |
++---------------------+-----------------+-------------+
+| one one one one one | two two two two | three three |
++---------------------+-----------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '50'})
+def test_table_list_formatter_env_maxwidth_resize_one():
+    sf = table.TableFormatter()
+    c = ('one', 'two', 'three')
+    d1 = (
+        'one one one one one',
+        'two two two two',
+        'three three')
+    data = [d1]
+    parsed_args = args()
+
+    # resize 1 column
+    expected = '''\
++----------------+-----------------+-------------+
+| one            | two             | three       |
++----------------+-----------------+-------------+
+| one one one    | two two two two | three three |
+| one one        |                 |             |
++----------------+-----------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+    assert len(actual.splitlines()[0]) == 50
+
+
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '45'})
+def test_table_list_formatter_env_maxwidth_resize_two():
+    sf = table.TableFormatter()
+    c = ('one', 'two', 'three')
+    d1 = (
+        'one one one one one',
+        'two two two two',
+        'three three')
+    data = [d1]
+    parsed_args = args()
+
+    # resize 2 columns
+    expected = '''\
++--------------+--------------+-------------+
+| one          | two          | three       |
++--------------+--------------+-------------+
+| one one one  | two two two  | three three |
+| one one      | two          |             |
++--------------+--------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+    assert len(actual.splitlines()[0]) == 45
+
+
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '40'})
+def test_table_list_formatter_env_maxwidth_resize_all():
+    sf = table.TableFormatter()
+    c = ('one', 'two', 'three')
+    d1 = (
+        'one one one one one',
+        'two two two two',
+        'three three')
+    data = [d1]
+    parsed_args = args()
+
+    # resize all columns
+    expected = '''\
++------------+------------+------------+
+| one        | two        | three      |
++------------+------------+------------+
+| one one    | two two    | three      |
+| one one    | two two    | three      |
+| one        |            |            |
++------------+------------+------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+    assert len(actual.splitlines()[0]) == 40
+
+
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '8'})
+def test_table_list_formatter_env_maxwidth_resize_all_tiny():
+    sf = table.TableFormatter()
+    c = ('one', 'two', 'three')
+    d1 = (
+        'one one one one one',
+        'two two two two',
+        'three three')
+    data = [d1]
+    parsed_args = args()
+
+    # resize all columns limited by min_width=8
+    expected = '''\
++----------+----------+----------+
+| one      | two      | three    |
++----------+----------+----------+
+| one one  | two two  | three    |
+| one one  | two two  | three    |
+| one      |          |          |
++----------+----------+----------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+    # 3 columns each 8 wide, plus table spacing and borders
+    expected_width = 11 * 3 + 1
+    assert len(actual.splitlines()[0]) == expected_width
+
+
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '42'})
+def test_table_list_formatter_env_maxwidth_args_big():
+    sf = table.TableFormatter()
+    c = ('one', 'two', 'three')
+    d1 = (
+        'one one one one one',
+        'two two two two',
+        'three three')
+    data = [d1]
+    parsed_args = args(666)
+
+    expected = '''\
++---------------------+-----------------+-------------+
+| one                 | two             | three       |
++---------------------+-----------------+-------------+
+| one one one one one | two two two two | three three |
++---------------------+-----------------+-------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
+
+
+@mock.patch.dict(os.environ, {'CLIFF_MAX_TERM_WIDTH': '42'})
+def test_table_list_formatter_env_maxwidth_args_tiny():
+    sf = table.TableFormatter()
+    c = ('one', 'two', 'three')
+    d1 = (
+        'one one one one one',
+        'two two two two',
+        'three three')
+    data = [d1]
+    parsed_args = args(40)
+
+    expected = '''\
++------------+------------+------------+
+| one        | two        | three      |
++------------+------------+------------+
+| one one    | two two    | three      |
+| one one    | two two    | three      |
+| one        |            |            |
++------------+------------+------------+
+'''
+    output = StringIO()
+    sf.emit_list(c, data, output, parsed_args)
+    actual = output.getvalue()
+    assert expected == actual
 
 
 @mock.patch('cliff.utils.terminal_width')
