c6d258da1ca18b9330f5b0554f57e8f67542b9fe
Rajath Agasthya <rajathagasthya@gmail.com>
2017-06-29 01:09:14 -0700

    Run hooks for DisplayCommandBase
    Command base class has provisions to run hooks for a command in
    its run() method. We need to do the same for the
    DisplayCommandBase class since it does not call super().run()
    
    Change-Id: Ic5481523d4bd919fe7fb10e00330dea2ff688ec4

--- cliff/command.py
+++ cliff/command.py
@@ -133,12 +133,36 @@ class Command(object):
 
         Return the value returned by :meth:`take_action` or 0.
         """
+        self._run_before_hooks(parsed_args)
+        return_code = self.take_action(parsed_args) or 0
+        self._run_after_hooks(parsed_args, return_code)
+        return return_code
+
+    def _run_before_hooks(self, parsed_args):
+        """Calls before() method of the hooks.
+
+        This method is intended to be called from the run() method before
+        take_action() is called.
+
+        This method should only be overriden by developers creating new
+        command base classes and only if it is necessary to have different
+        hook processing behavior.
+        """
         for hook in self._hooks:
             hook.obj.before(parsed_args)
-        return_code = self.take_action(parsed_args) or 0
+
+    def _run_after_hooks(self, parsed_args, return_code):
+        """Calls after() method of the hooks.
+
+        This method is intended to be called from the run() method after
+        take_action() is called.
+
+        This method should only be overriden by developers creating new
+        command base classes and only if it is necessary to have different
+        hook processing behavior.
+        """
         for hook in self._hooks:
             hook.obj.after(parsed_args, return_code)
-        return return_code
 
 
 class _SmartHelpFormatter(_argparse.HelpFormatter):
