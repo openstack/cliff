d1dc0902a9a55378d13f7b2ba9238152e289a5af
Monty Taylor <mordred@inaugust.com>
2020-06-06 12:15:30 -0500

    Remove unneeded tests
    These are for <3.2
    
    Change-Id: I376e3601d5799e11590b2814714655692159de99

--- cliff/tests/test_utils.py
+++ cliff/tests/test_utils.py
@@ -13,9 +13,7 @@
 #  under the License.
 
 import os
-import struct
 import sys
-import unittest
 
 from unittest import mock
 
@@ -32,8 +30,6 @@ class TestTerminalWidth(base.TestBase):
         if width is not None:
             self.assertIsInstance(width, int)
 
-    @unittest.skipIf(not hasattr(os, 'get_terminal_size'),
-                     'only needed for python 3.3 onwards')
     @mock.patch('cliff.utils.os')
     def test_get_terminal_size(self, mock_os):
         ts = os.terminal_size((10, 5))
@@ -42,36 +38,4 @@ class TestTerminalWidth(base.TestBase):
         self.assertEqual(10, width)
         mock_os.get_terminal_size.side_effect = OSError()
         width = utils.terminal_width(sys.stdout)
-        self.assertIs(None, width)
-
-    @unittest.skipIf(hasattr(os, 'get_terminal_size'),
-                     'only needed for python 3.2 and before')
-    @mock.patch('fcntl.ioctl')
-    def test_ioctl(self, mock_ioctl):
-        mock_ioctl.return_value = struct.pack('hhhh', 57, 101, 0, 0)
-        width = utils.terminal_width(sys.stdout)
-        self.assertEqual(101, width)
-        mock_ioctl.side_effect = IOError()
-        width = utils.terminal_width(sys.stdout)
-        self.assertIs(None, width)
-
-    @unittest.skipIf(hasattr(os, 'get_terminal_size'),
-                     'only needed for python 3.2 and before')
-    @mock.patch('cliff.utils.ctypes')
-    @mock.patch('sys.platform', 'win32')
-    def test_windows(self, mock_ctypes):
-        mock_ctypes.create_string_buffer.return_value.raw = struct.pack(
-            'hhhhHhhhhhh', 101, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-        mock_ctypes.windll.kernel32.GetStdHandle.return_value = -11
-        mock_ctypes.windll.kernel32.GetConsoleScreenBufferInfo.return_value = 1
-
-        width = utils.terminal_width(sys.stdout)
-        self.assertEqual(101, width)
-
-        mock_ctypes.windll.kernel32.GetConsoleScreenBufferInfo.return_value = 0
-
-        width = utils.terminal_width(sys.stdout)
-        self.assertIs(None, width)
-
-        width = utils.terminal_width('foo')
         self.assertIs(None, width)
